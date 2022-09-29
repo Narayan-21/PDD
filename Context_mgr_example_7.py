5de7700d8737e81abcc311943381ecfa25b0fe7f
Narayan-21 <naaidjan.19@gmail.com>
2022-09-29 21:58:48 +0530

    Context Manager Example

--- Context_mgr_example_7.py
+++ Context_mgr_example_7.py
@@ -1,5 +1,8 @@
 # Using contextmanager from contextlib module
 from contextlib import contextmanager
+from time import perf_counter, sleep
+
+# Example-1
 
 @contextmanager
 def open_file(fname, mode='r'):
@@ -13,3 +16,22 @@ def open_file(fname, mode='r'):
 
 with open_file('test.txt','r') as f:
     print(f.readlines())
+
+
+# Example-2
+@contextmanager
+def timer():
+    stats = dict()
+    start = perf_counter()
+    stats['Start']=start
+    try:
+        yield stats
+    finally:
+        end = perf_counter()
+        stats['end']=end
+        stats['elapsed']=end-start
+
+with timer() as sts:
+    sleep(3)
+
+print(sts)
